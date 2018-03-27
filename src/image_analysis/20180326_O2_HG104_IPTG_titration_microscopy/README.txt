c6261fccc4548c6b19b8b51f647e434936bcab4e
mrazomej <manuel.razo.m@gmail.com>
2018-03-26 23:06:16 -0700

    add image analysis from 20180326

---
+++ src/image_analysis/20180326_O2_HG104_IPTG_titration_microscopy/README.txt
@@ -0,0 +1,32 @@
+#Date : 2018-03-26
+#Equipment : Artemis Nikon microscope
+#User : mrazomej
+#Description : 
+#IPTG titration of the O2 - HG104 strain.
+#Strains :
+#> HG105 galK::25 / pZS3-mCherry (auto)
+#> HG105 galK::25-O2+11-YFP / pZS4-mCherry (delta)
+#> HG104 galK::25-O2+11-YFP / pZS4-mCherry
+#
+#The strains were grown overnight in tubes in 3 mL of LB + spec + kan. 
+#Next morning they were diluted 1:1000 into 0.5 mL of M9 + 0.5% glucose 
+#also in deep 96-well plates.
+#The auto and delta strains were grown without IPTG
+#The analysis strain was grown with the following IPTG concentrations
+#IPTG concentrations:
+#0uM, 0.1uM, 5uM, 10uM, 25uM, 50uM, 75uM, 100uM, 250uM, 500uM, 1000uM, 5000uM
+#After 8 hours the cells were diluted 1:10 into M9 + glucose and imaged
+#using 2% agar pads also of M9 media.
+#
+#Microscope settings:
+#100x Oil objective
+#Exposure time:
+#> Brightfield : 3 ms
+#> mCherry : 18 ms
+#> YFP : 9 ms
+#
+#Comments :
+# It is possible that the media is not the right one. I had to do a much
+# smaller dilution (1:3) to get the right number of cells and for a
+# different project my labmates have found that my media seems to make
+# cells grow slower.
---
+++ src/image_analysis/20180326_O2_HG104_IPTG_titration_microscopy/processing.py
@@ -0,0 +1,137 @@
+import os
+import glob
+
+# Our numerical workhorses
+import numpy as np
+import pandas as pd
+import scipy.special
+
+# Import the project utils
+import sys
+sys.path.insert(0, '../')
+import image_analysis_utils as im_utils
+
+# Useful plotting libraries
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib as mpl
+import seaborn as sns
+
+# Image analysis libraries
+import skimage.io
+import skimage.filters
+import skimage.segmentation
+import scipy.ndimage
+
+# Set plotting style
+im_utils.set_plotting_style()
+
+#============================================================================== 
+# METADATA
+#============================================================================== 
+
+DATE = 20180326
+USERNAME = 'mrazomej'
+OPERATOR = 'O2'
+BINDING_ENERGY = -13.9
+REPRESSORS = (0, 0, 11)
+IPDIST = 0.160  # in units of µm per pixel
+STRAINS = ['auto', 'delta', 'HG104']
+IPTG_RANGE = (0, 0.1, 5, 10, 25, 50, 75, 100, 250, 500, 1000, 5000)
+# Extra feature because my mistake when naming files
+IPTG_NAMES = ('0', '0.1', '5', '10', '25', '50', '75', '100', '250', '500', 
+              '1000', '5000')
+IPTG_DICT = dict(zip(IPTG_NAMES, IPTG_RANGE))
+
+#============================================================================== 
+
+# Define the data directory.
+data_dir = '../../../data/microscopy/' + str(DATE) + '/'
+
+# Glob the profile and noise images.
+yfp_glob = glob.glob(data_dir + '*YFP_profile*/*.tif')
+noise_glob = glob.glob(data_dir + '*noise*/*.tif')
+
+# Load the images as collections
+yfp_profile = skimage.io.ImageCollection(yfp_glob)
+noise_profile = skimage.io.ImageCollection(noise_glob)
+
+# Need to split the noise profile image into the two channels
+noise_yfp = [noise_profile[i] for i, _ in enumerate(noise_profile)]
+
+# Generate averages and plot them. 
+yfp_avg = im_utils.average_stack(yfp_profile)
+
+yfp_noise = im_utils.average_stack(noise_yfp)
+
+with sns.axes_style('white'):
+    fig, ax =  plt.subplots(1, 2, figsize=(6,3))
+    ax = ax.ravel()
+    ax[0].imshow(yfp_avg, cmap=plt.cm.viridis)
+    ax[0].set_title('yfp profile')
+    ax[1].imshow(yfp_noise, cmap=plt.cm.Greens_r)
+    ax[1].set_title('yfp noise')
+plt.tight_layout()
+plt.savefig('./outdir/background_correction.png')
+
+#============================================================================== 
+
+# Iterate through each strain and concentration to make the dataframes.
+dfs = []
+
+# Select random IPTG and random strain to print the example segmentation
+ex_iptg = np.random.choice(IPTG_RANGE)
+ex_strain = STRAINS[-1]
+for i, st in enumerate(STRAINS):
+    print(st)
+    for j, name in enumerate(IPTG_NAMES):
+        iptg = IPTG_DICT[name]
+        # Load the images
+        if (iptg==0) & (st != STRAINS[-1]):
+            images = glob.glob(data_dir + '*' + st + '_*/*.tif')
+            
+        else:
+            images = glob.glob(data_dir + '*' + st + '*_' + name +
+                           'uMIPTG*/*.ome.tif')
+            
+        if len(images) is not 0:
+
+            ims = skimage.io.ImageCollection(images)
+            # Select random image to print example segmentation
+            ex_no = np.random.choice(np.arange(0, len(images) - 1))    
+            for z, x in enumerate(ims):
+                _, m, y = im_utils.ome_split(x)
+                y_flat = im_utils.generate_flatfield(y, yfp_noise, yfp_avg)
+    
+                # Segment the mCherry channel.
+                m_seg = im_utils.log_segmentation(m, label=True)
+
+                # Print example segmentation for the random image
+                if (st==ex_strain) & (iptg == ex_iptg) & (z == ex_no):
+                    merge = im_utils.example_segmentation(m_seg, _, 10/IPDIST)
+                    skimage.io.imsave('./outdir/example_segmentation.png', merge)
+    
+                 # Extract the measurements.
+                try:
+                    im_df =im_utils.props_to_df(m_seg, physical_distance=IPDIST,
+                                                 intensity_image=y_flat)
+                except ValueError:
+                    break
+    
+                # Add strain and  IPTG concentration information.
+                im_df.insert(0, 'IPTG_uM', iptg)
+                im_df.insert(0, 'repressors', REPRESSORS[i])
+                im_df.insert(0, 'rbs', st)
+                im_df.insert(0, 'binding_energy', BINDING_ENERGY)
+                im_df.insert(0, 'operator', OPERATOR)
+                im_df.insert(0, 'username', USERNAME)
+                im_df.insert(0, 'date', DATE)
+    
+                # Append the dataframe to the global list.
+                dfs.append(im_df)
+
+# Concatenate the dataframe
+df_im = pd.concat(dfs, axis=0)
+df_im.to_csv('./outdir/' + str(DATE) + '_' + OPERATOR + '_' +\
+               STRAINS[-1] + '_raw_segmentation.csv', index=False)
+
